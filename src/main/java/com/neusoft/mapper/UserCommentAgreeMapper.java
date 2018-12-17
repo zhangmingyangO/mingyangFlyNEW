feb2b4e051dd8aca8ebd28da8c1272eefac8b830
zhangmingyang <1154540719@qq.com>
2018-12-17 14:19:42 +0800

    完成点赞功能

---
+++ src/main/java/com/neusoft/mapper/UserCommentAgreeMapper.java
@@ -0,0 +1,20 @@
+package com.neusoft.mapper;
+
+import com.neusoft.domain.UserCommentAgree;
+
+import java.util.Map;
+
+public interface UserCommentAgreeMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(UserCommentAgree record);
+
+    int insertSelective(UserCommentAgree record);
+
+    UserCommentAgree selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(UserCommentAgree record);
+
+    int updateByPrimaryKey(UserCommentAgree record);
+    int deleteByUserIDAndCommentID(Map<String,Object> params);
+}
